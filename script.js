d97f48540fbf4a833d3334e8b20fece2524fc517
elvis <kariuki.elvis@student.moringaschool.com>
2026-03-10 12:51:52 +0300

    styled the web page

---
+++ script.js
@@ -0,0 +1,13 @@
+function learnMore(){
+
+alert("Welcome to Elvis Tech Solutions! Explore our services.");
+
+}
+
+function sendMessage(e){
+
+e.preventDefault();
+
+alert("Your message has been sent successfully!");
+
+}
